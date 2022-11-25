d9168ef8a6bda1c755cd5bb00321a26d277f4095
febiasm <trevorfebias@gmail.com>
2022-11-25 18:03:06 +0300

    dine

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// code your solution here
+function saturdayFun ( skate= 'roller-skate' ){
+    return `This Saturday, I want to ${skate}!`
+}
+saturdayFun('go home')
+
+function mondayWork ( job = 'go to the office'){
+    return `This Monday, I will ${job}.`
+}
+ mondayWork ('work from home')
+
+function wrapAdjective(param0 = "*") {
+    function message(string = "special") {
+      return `You are ${param0}${string}${param0}!`;
+    };
+    return message;
+  };
+wrapAdjective("||");
+result("a hard worker");
